35d1c96cfb026e998f144bc789579c52397f0980
HusHDreamer <2237269163@qq.com>
2022-02-28 18:08:58 +0800

    hello world!

--- src/hello/hellocode.java
+++ src/hello/hellocode.java
@@ -1,5 +1,10 @@
 package hello;
 
 public class hellocode {
-	System.out.println("hello world");
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("hello world!");
+	}
+
 }
